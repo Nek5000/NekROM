941339524b0e2010593b78b2c1c217122b9788cf
Kento Kaneko <kaneko2@illinois.edu>
2018-08-24 07:53:29 -0500

    added matlab code to advance time using ROM

---
+++ pod/advance.m
@@ -0,0 +1,53 @@
+% advance state using cten, amat, bmat, c1mat, c2mat, avec, cvec
+
+t = dlmread('cten');
+n = nthroot(length(t),3);
+c0 = reshape(t,n,n,n);
+
+t = dlmread('amat');
+n = sqrt(length(t));
+a0 = reshape(t,n,n);
+
+t = dlmread('bmat');
+n = sqrt(length(t));
+b = reshape(t,n,n);
+
+t = dlmread('c1mat');
+n = sqrt(length(t));
+c1 = reshape(t,n,n);
+
+t = dlmread('c2mat');
+n = sqrt(length(t));
+c2 = reshape(t,n,n);
+
+a1 = dlmread('avec');
+c3 = dlmread('cvec');
+
+dt = 1e-5;
+re = 1000;
+nsteps = 200000;
+iostep = 10000;
+
+beta0 = 1;
+
+u0 = zeros(n,1);
+
+u = u0
+
+for istep = 1:nsteps
+    helm = (b * beta0 / dt + a0 / re);
+
+    t = zeros(n,1);
+    for i = 1:n
+        t = t + reshape(c0(:,i,:),[n,n]) * u(i);
+    end
+
+    rhs = - t * u + b * u / dt - c1 * u - c2 * u - a1 - c3;
+    u = helm \ rhs
+    if (mod(istep,iostep) == 0)
+        fname = strcat(num2str(istep/iostep),'.out')
+        fid = fopen(fname,'w');
+        fprintf(fid,'%d\n',u);
+        fclose(fid);
+    end
+end
