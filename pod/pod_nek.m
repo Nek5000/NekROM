a597231b8e1dc883f35fb4334938bfef4c765b28
Kento Kaneko <kaneko2@illinois.edu>
2018-08-25 10:34:23 -0500

    sort singular values

--- pod/pod_nek.m
+++ pod/pod_nek.m
@@ -5,6 +5,10 @@ A=reshape(fort,n,n);
 
 [V,D]=eig(A); D=diag(D);
 
+[D,i]=sort(D,'descend');
+
+V=V(:,i);
+
 i=1:n;
 
 semilogy(i,D,'ro-')
@@ -13,6 +17,7 @@ semilogy(i,D,'ro-')
 N=20;
 D(1:N)'
 V=V(:,1:N); V=reshape(V,n*N,1);
+
 
 fid = fopen('evectors.dat','w');
 fprintf(fid,'%22.13e\n',V);
