4cf5c8b07a2605c6873fd2e2b83030580343c711
Sean <r04246004@ntu.edu.tw>
2018-08-25 13:23:07 -0500

    BDF3/EXT3 completed

--- pod/advance.m
+++ pod/advance.m
@@ -14,19 +14,20 @@ b0 = reshape(t,n,n);
 
 nb = n - 1
 
-aa = a0(2:n,2:n);
-a = diag(a0(2:n,2:n)); % left-handed side A
-b = b0(2:n,2:n); % left-handed side B
+a = diag(a0(2:n,2:n)) % left-handed side A
+b = b0(2:n,2:n) % left-handed side B
 
-%c = c0(1:nb,1:nb,1:nb)
 a1 = reshape(a0(1,2:n),[nb,1]);
-c1 = reshape(c0(2:n,1,2:n),[nb,nb]);
-c2 = reshape(c0(2:n,2:n,1),[nb,nb]);
-c3 = reshape(c0(2:n,1,1),[nb,1]);
+%c = c0(1:nb,1:nb,1:nb)
+%c1 = reshape(c0(2:n,1,1:n),[nb,n]);
+%c2 = reshape(c0(2:n,1:n,1),[nb,n]);
+%c3 = reshape(c0(1,1,2:n),[nb,1]);
 
 dt = 1e-5;
-nsteps = 2e5;
-iostep = 1e4;
+nsteps = 10;
+iostep = 1;
+%nsteps = 2e5;
+%iostep = 1e4;
 
 %nsteps = 2000;
 %iostep = 100;
@@ -41,7 +42,7 @@ u(1:n,1:(nsteps/iostep)) = 0
 u(1,1:(nsteps/iostep)) = 1
 
 % do BDF1 to get u1, u2, u3
-for istep = 1:4
+for istep = 1:3
     helm = (b * beta0 / dt + a / re);
 
     t = zeros(nb,n);
@@ -51,16 +52,57 @@ for istep = 1:4
 
     rhs = b0(2:n,1:n) * u(:,istep) / dt - a1 / re;
     rhs = rhs - t * u(:,istep); % advection contributions
+%   rhs = rhs - c1 * u - c2 * u; % advection contributions
     rhs = rhs - a0(2:n,1:n) * e0 / re - b0(2:n,1:n) * e0 / dt;
     tmp = helm \ rhs;
-    u(2:n,istep) = tmp;
+    u(2:n,istep+1) = tmp;
+
+    if (mod(istep,iostep) == 0)
+        fname = strcat(num2str(istep/iostep),'.out')
+        fid = fopen(fname,'w');
+        fprintf(fid,'%d\n',u(1:n,istep));
+        fclose(fid);
+        u(1:n,istep)
+    end
+
 end
 
-%    U = [u0,u1,u2,u3]
-%    if (mod(istep,iostep) == 0)
-%        fname = strcat(num2str(istep/iostep),'.out')
-%        fid = fopen(fname,'w');
-%        fprintf(fid,'%d\n',u);
-%        fclose(fid);
-%        u
-%    end
+% BDF3/EXT3 coefficients
+beta = [11/6,-3,3/2,-1/3]
+alpha = [3,-3,1]
+
+% do BDF3 
+for istep = 4:(nsteps/iostep)
+    helm = (b * beta(4) / dt + a / re);
+
+    t1 = zeros(nb,n);
+    t2 = zeros(nb,n);
+    t3 = zeros(nb,n);
+    for i = 1:n
+        t3 = t3 + reshape(c0(2:n,:,i),[nb,n]) * u(i,istep-3);
+        t2 = t2 + reshape(c0(2:n,:,i),[nb,n]) * u(i,istep-2);
+        t1 = t1 + reshape(c0(2:n,:,i),[nb,n]) * u(i,istep-1);
+    end
+
+
+    rhs = -beta(1) * b0(2:n,1:n) * u(:,istep-3) / dt ;
+    rhs = rhs - beta(2) * b0(2:n,1:n) * u(:,istep-2) / dt ;
+    rhs = rhs - beta(3) * b0(2:n,1:n) * u(:,istep-1) / dt ;
+    rhs = rhs - a1 / re;
+
+    rhs = rhs - alpha(1) * t3 * u(:,istep-3); % advection contributions
+    rhs = rhs - alpha(2) * t2 * u(:,istep-2); % advection contributions
+    rhs = rhs - alpha(3) * t1 * u(:,istep-1); % advection contributions
+%   rhs = rhs - c1 * u - c2 * u; % advection contributions
+    rhs = rhs - a0(2:n,1:n) * e0 / re - beta(4) * b0(2:n,1:n) * e0 / dt;
+    tmp = helm \ rhs;
+    u(2:n,istep) = tmp;
+
+    if (mod(istep,iostep) == 0)
+        fname = strcat(num2str(istep/iostep),'.out')
+        fid = fopen(fname,'w');
+        fprintf(fid,'%d\n',u(1:n,istep));
+        fclose(fid);
+        u(1:n,istep)
+    end
+end
