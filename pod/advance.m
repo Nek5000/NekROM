7fafac0d6ce396219d9e36490039c74b7faffbc0
Sean <r04246004@ntu.edu.tw>
2018-08-25 10:41:25 -0500

    specify a1 c1 c2 c3 matrix

--- pod/advance.m
+++ pod/advance.m
@@ -15,8 +15,12 @@ b0 = reshape(t,n,n);
 nb = n - 1
 
 a = diag(a0(1:nb,1:nb))
+a1 = a0(1,1:nb)
 b = b0(1:nb,1:nb)
 c = c0(1:nb,1:nb,1:nb)
+c1 = c0(1:nb,1,1:nb)
+c2 = c0(1:nb,1:nb,1)
+c3 = c0(1,1,1:nb)
 
 dt = 1e-5;
 nsteps = 2e5;
@@ -28,23 +32,26 @@ iostep = 1e4;
 re = 1e3;
 beta0 = 1;
 
-%u0 = zeros(n,1);
-%
-%u = u0
-%
-%for istep = 1:nsteps
-%    helm = (b * beta0 / dt + a0 / re);
-%
-%    t = zeros(n,1);
-%    for i = 1:n
-%        t = t + reshape(c0(:,i,:),[n,n]) * u(i);
-%    end
-%
-%    rhs = b * u / dt - a1 / re;
-%    rhs = rhs - t * u - c1 * u - c2 * u; % advection contributions
+u0 = zeros(n,1);
+
+u = u0
+
+% do BDF1 to get u1, u2, u3
+for istep = 1:3
+    helm = (b * beta0 / dt + a / re);
+
+    t = zeros(n,1);
+    for i = 1:n
+        t = t + reshape(c(:,i,:),[n,n]) * u(i);
+    end
+
+    rhs = b * u / dt - a1 / re;
+    rhs = rhs - t * u - c1 * u - c2 * u; % advection contributions
 %   rhs = rhs - c1 * u - c2 * u; % advection contributions
-%    rhs = rhs - c3; % not in Patera 2017
+    rhs = rhs - c3; % not in Patera 2017
 %    u = helm \ rhs;
+end
+    U = [u0,u1,u2,u3]
 %    if (mod(istep,iostep) == 0)
 %        fname = strcat(num2str(istep/iostep),'.out')
 %        fid = fopen(fname,'w');
@@ -52,4 +59,3 @@ beta0 = 1;
 %        fclose(fid);
 %        u
 %    end
-%end
