9e641381024dc62a7df65e8ccb50391fcc4afda7
Kento Kaneko <kaneko2@illinois.edu>
2018-08-25 09:02:35 -0500

    comment out time-stepping for now

--- pod/advance.m
+++ pod/advance.m
@@ -24,28 +24,28 @@ iostep = 1e4;
 re = 1e3;
 beta0 = 1;
 
-u0 = zeros(n,1);
-
-u = u0
-
-for istep = 1:nsteps
-    helm = (b * beta0 / dt + a0 / re);
-
-    t = zeros(n,1);
-    for i = 1:n
-        t = t + reshape(c0(:,i,:),[n,n]) * u(i);
-    end
-
-    rhs = b * u / dt - a1 / re;
-    rhs = rhs - t * u - c1 * u - c2 * u; % advection contributions
+%u0 = zeros(n,1);
+%
+%u = u0
+%
+%for istep = 1:nsteps
+%    helm = (b * beta0 / dt + a0 / re);
+%
+%    t = zeros(n,1);
+%    for i = 1:n
+%        t = t + reshape(c0(:,i,:),[n,n]) * u(i);
+%    end
+%
+%    rhs = b * u / dt - a1 / re;
+%    rhs = rhs - t * u - c1 * u - c2 * u; % advection contributions
 %   rhs = rhs - c1 * u - c2 * u; % advection contributions
-    rhs = rhs - c3; % not in Patera 2017
-    u = helm \ rhs;
-    if (mod(istep,iostep) == 0)
-        fname = strcat(num2str(istep/iostep),'.out')
-        fid = fopen(fname,'w');
-        fprintf(fid,'%d\n',u);
-        fclose(fid);
-        u
-    end
-end
+%    rhs = rhs - c3; % not in Patera 2017
+%    u = helm \ rhs;
+%    if (mod(istep,iostep) == 0)
+%        fname = strcat(num2str(istep/iostep),'.out')
+%        fid = fopen(fname,'w');
+%        fprintf(fid,'%d\n',u);
+%        fclose(fid);
+%        u
+%    end
+%end
