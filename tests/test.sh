76bdd804093bf439b4e441dfde415128bda302ae
Kento Kaneko <kaneko2@illinois.edu>
2018-10-20 18:08:40 -0500

    added back GRAMMIAN

--- tests/test.sh
+++ tests/test.sh
@@ -10,9 +10,9 @@ $ROOT_DIR/bin/gsnaps baf
 $ROOT_DIR/bin/gops   baf
 
 case "$TEST" in
-#   GRAMMIAN_UNIT)
-#       $ROOT_DIR/tests/grammian_test.sh
-#       ;;
+    GRAMMIAN_UNIT)
+        $ROOT_DIR/tests/grammian_test.sh
+        ;;
     BAFF_COMP)
         $ROOT_DIR/tests/comp-baff-test
         ;;
