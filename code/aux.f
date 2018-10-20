05ba7c90096273f0301abfa3514921052ba3c043
Kento Kaneko <kaneko2@illinois.edu>
2018-10-20 10:26:44 -0500

    added more read and dump subroutines

--- code/aux.f
+++ code/aux.f
@@ -194,3 +194,198 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
+      subroutine readc0(c0,n)
+
+      include 'SIZE'
+      include 'PARALLEL'
+
+      real c0(n)
+
+      if (nid.eq.(np-1)) then
+         open (unit=12,file='cten')
+         read (12,*) (c0(k),k=1,n)
+         close (unit=12)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine readab(a0,b0,n)
+
+      include 'SIZE'
+
+      real a0(n),b0(n)
+
+      common /scrk5/ w(lx1*ly1*lz1*lelt)
+
+      call rzero(a0,n)
+      call rzero(b0,n)
+
+      if (nid.eq.0) then
+         open (unit=12,file='amat')
+         read (12,*) (a0(k),k=1,n)
+         close (unit=12)
+
+         open (unit=12,file='bmat')
+         read (12,*) (b0(k),k=1,n)
+         close (unit=12)
+      endif
+
+      call gop(a0,w,'+  ',n)
+      call gop(b0,w,'+  ',n)
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine readic(ic,n)
+
+      include 'SIZE'
+
+      real ic(n)
+
+      if (nid.eq.0) then
+         open (unit=12,file='ic')
+         read (12,*) (ic(k),k=1,n)
+         close (unit=12)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine readbasis(ub,vb,wb,nb)
+
+      include 'SIZE'
+
+      parameter (lt=lx1*ly1*lz1*lelt)
+
+      real ub(lt,0:nb), vb(lt,0:nb), wb(lt,0:nb)
+
+      n=lx1*ly1*lz1*nelt
+
+      if (nid.eq.0) then
+         open (unit=12,file='basis')
+
+         do j=0,nb
+            read (12,*) (ub(i,j),i=1,n)
+         enddo
+
+         do j=0,nb
+            read (12,*) (vb(i,j),i=1,n)
+         enddo
+
+         if (ldim.eq.3) then
+         do j=0,nb
+            read (12,*) (wb(i,j),i=1,n)
+         enddo
+         endif
+
+         close (unit=12)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine dumpbasis(ub,vb,wb,nb)
+
+      include 'SIZE'
+
+      parameter (lt=lx1*ly1*lz1*lelt)
+
+      real ub(lt,0:nb), vb(lt,0:nb), wb(lt,0:nb)
+
+      n=lx1*ly1*lz1*nelt
+
+      if (nid.eq.0) then
+         open (unit=12,file='basis')
+
+         do j=0,nb
+         do i=1,n
+            write (12,*) ub(i,j)
+         enddo
+         enddo
+
+         do j=0,nb
+         do i=1,n
+            write (12,*) ub(i,j)
+         enddo
+         enddo
+
+         if (ldim.eq.3) then
+         do j=0,nb
+         do i=1,n
+            write (12,*) ub(i,j)
+         enddo
+         enddo
+         endif
+
+         close (unit=12)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine dumptens(c,nb)
+
+      include 'SIZE'
+
+      real c(0:nb,0:nb,0:nb)
+
+      if (nid.eq.0) then
+         open (unit=50,file='cten')
+
+         do i=0,(nb+1)**3-1
+            write (50,*) c(i,0,0)
+         enddo
+
+         close (unit=50)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine dumpmats(a,b,nb)
+
+      include 'SIZE'
+
+      real a(0:nb,0:nb), b(0:nb,0:nb)
+
+      if (nid.eq.0) then
+         open (unit=50,file='amat')
+
+         do i=0,(nb+1)**2-1
+            write (50,*) a(i,1)
+         enddo
+
+         close (unit=50)
+
+         open (unit=50,file='bmat')
+
+         do i=0,(nb+1)**2-1
+            write (50,*) b(i,1)
+         enddo
+
+         close (unit=50)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine dumpic(ic)
+
+      include 'SIZE'
+
+      real ic(0:nb)
+
+      if (nid.eq.0) then
+         open (unit=50,file='ic')
+
+         do i=0,nb
+            write (50,*) ic(i)
+         enddo
+
+         close (unit=50)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
