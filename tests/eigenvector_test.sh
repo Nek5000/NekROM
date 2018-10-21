39cc23ec87ac2fb2937aa13312ff971768cf8204
Kento Kaneko <kaneko2@illinois.edu>
2018-10-21 08:47:50 -0500

    removed deprecated scripts

--- tests/eigenvector_test.sh
+++
@@ -1,8 +0,0 @@
-test_name=eigenvector_test $ROOT_DIR/tests/test_template.sh
-ls $ROOT_DIR/data/baf | sed 's/^/..\/..\/data\/baf\//g' > file.list
-$SOURCE_ROOT/bin/makenek test
-$SOURCE_ROOT/bin/genmap << Z
-baf
-.01
-Z
-./nek5000
