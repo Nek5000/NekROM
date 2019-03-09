4320d21dda3f1847d09e6d3b44f07c45922f4238
Kento Kaneko <kaneko2@illinois.edu>
2019-03-09 16:25:55 -0600

    added theta setting and error estimate computation

--- code/ei.f
+++ code/ei.f
@@ -54,3 +54,64 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
+      subroutine sett() ! set thetas
+
+      include 'SIZE'
+      include 'MOR'
+      include 'MASS'
+
+      parameter (lt=lx1*ly1*lz1*lelt)
+
+      common /scrsets/ g1(lt),g2(lt),g3(lt),h1(lt),h2(lt)
+
+      esum=0.
+
+      n=lx1*ly1*lz1*nelt
+
+      call rone(h1,n)
+      call rzero(h2,n)
+
+      rinstep=1./real(ad_nsteps)
+
+      call rzero(thb,nb+1)
+      call add2s2(thb,uj(0,1),-5./6,nb+1)
+      call add2s2(thb,uj(0,2), 1./6,nb+1)
+      call add2s2(thb,uj(0,3),-1./3,nb+1)
+      call add2s2(thb,uj(0,4), 1./3,nb+1)
+      call add2s2(thb,uj(0,5),-7./6,nb+1)
+      call add2s2(thb,uj(0,6),11./6,nb+1)
+      s=ad_dt*rinstep
+      call cmult(thb,s,nb+1)
+
+      s=1./ad_re
+      call cmult2(tha,ua,s,nb+1)
+
+      call copy(thc,u2a,(nb+1)**2)
+
+      call add2s2(thc,u2j(0,0,2),-1.*rinstep,(nb+1)**2)
+      call add2s2(thc,u2j(0,0,5),-1.*rinstep,(nb+1)**2)
+      call add2s2(thc,u2j(0,0,6), 2.*rinstep,(nb+1)**2)
+
+      return
+      end
+c-----------------------------------------------------------------------
+      function eest() ! compute error estimate
+
+      include 'SIZE'
+      include 'MOR'
+
+      eest=0.
+
+      do j=0,nb
+         eest=eest+sigb(j)*thb(j)**2
+         eest=eest+siga(j)*tha(j)**2
+         do i=0,nb
+            eest=eest+sigc(i,j)*thc(i,j)**2
+         enddo
+      enddo
+
+      eest=sqrt(eest)
+
+      return
+      end
+c-----------------------------------------------------------------------
