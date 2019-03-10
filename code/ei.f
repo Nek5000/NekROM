1c1d76a92de309bd291d2daee6040d402591ced4
Kento Kaneko <kaneko2@illinois.edu>
2019-03-10 10:31:04 -0500

    added switching ability

--- code/ei.f
+++ code/ei.f
@@ -1,7 +1,9 @@
 c-----------------------------------------------------------------------
-      function csig(g1,g2,g3,h1,h2)
+      function csig(g1,g2,g3,h1,h2,op)
 
       include 'SIZE'
+
+      character*3 op
 
       parameter (lt=lx1*ly1*lz1*lelt)
 
@@ -11,12 +13,32 @@ c-----------------------------------------------------------------------
       tolh=1.e-5
       nmxhi=1000
 
-      call ophinv(r1,r2,r3,g1,g2,g3,h1,h2,tolh,nmxhi)
+      call invop(r1,r2,r3,g1,g2,g3,h1,h2,tolh,nmxhi,op)
 
       mio=nio
       nio=-1
-      csig=h10prod(r1,r2,r3,r1,r2,r3,h1,h2)
+      csig=vecprod(r1,r2,r3,r1,r2,r3,h1,h2,op)
       nio=mio
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine invop(r1,r2,r3,g1,g2,g3,h1,h2,tolh,nmxhi,op)
+
+      include 'SIZE'
+
+      real g1(1),g2(1),g3(1),h1(1),h2(1)
+      real r1(1),r2(1),r3(1)
+
+      character*3 op
+
+      if (op.eq.'H10') then
+         call ophinv(r1,r2,r3,g1,g2,g3,h1,h2,tolh,nmxhi)
+      else if (op.eq.'L2 ') then
+         call opbinv1(r1,r2,r3,g1,g2,g3,1.)
+      else
+         call exitti('did not provide supported operator$')
+      endif
 
       return
       end
@@ -32,7 +54,10 @@ c-----------------------------------------------------------------------
 
       common /scrsets/ g1(lt),g2(lt),g3(lt),h1(lt),h2(lt)
 
+      character *3 op
+
       n=lx1*ly1*lz1*nelt
+      op='L2 '
 
       call rone(h1,n)
       call rzero(h2,n)
@@ -42,18 +67,18 @@ c-----------------------------------------------------------------------
          call col3(g1,ub(1,j),bm1,n)
          call col3(g2,vb(1,j),bm1,n)
          if (ldim.eq.3) call col3(g3,wb(1,j),bm1,n)
-         sigb(j)=csig(g1,g2,g3,h1,h2)
+         sigb(j)=csig(g1,g2,g3,h1,h2,op)
 
          call axhelm(g1,ub(1,j),h1,h2,1,1)
          call axhelm(g2,vb(1,j),h1,h2,1,1)
          if (ldim.eq.3) call axhelm(g3,wb(1,j),h1,h2,1,1)
-         siga(j)=csig(g1,g2,g3,h1,h2)
+         siga(j)=csig(g1,g2,g3,h1,h2,op)
 
          call setcnv_c(ub(1,j),vb(1,j),wb(1,j))
          do i=0,nb
             call setcnv_u(ub(1,i),vb(1,i),wb(1,i))
             call ccu(g1,g2,g3)
-            sigc(i,j)=csig(g1,g2,g3,h1,h2)
+            sigc(i,j)=csig(g1,g2,g3,h1,h2,op)
          enddo
       enddo
       if (param(94).gt.0) ifprojfld(1)=.true.
