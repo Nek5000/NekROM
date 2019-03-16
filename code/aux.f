92ec006a5d1dca44f9379b2175d3900dcd682999
Kento Kaneko <kaneko2@illinois.edu>
2019-03-16 13:33:04 -0500

    remove space

--- code/aux.f
+++ code/aux.f
@@ -1,5 +1,5 @@
 c-----------------------------------------------------------------------
-      subroutine opadd3 (a1,a2,a3,b1,b2,b3,c1,c2,c3)
+      subroutine opadd3(a1,a2,a3,b1,b2,b3,c1,c2,c3)
 
       include 'SIZE'
 
