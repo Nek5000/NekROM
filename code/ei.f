fcc88d96d0c51703f89d31bfb75e669635725b9a
Kento Kaneko <kaneko2@illinois.edu>
2019-03-24 14:26:45 -0500

    specify that error indicator is based on diagonal

--- code/ei.f
+++ code/ei.f
@@ -1,5 +1,53 @@
 c-----------------------------------------------------------------------
-      function csig(g1,g2,g3,h1,h2,op)
+      subroutine rom_update_v_ei
+
+      include 'SIZE'
+      include 'TOTAL'
+      include 'MOR'
+
+      integer icalld
+      save    icalld
+      data    icalld /0/
+
+      common /rom_update/ rom_time
+
+      if (icalld.eq.0) then
+         call opcopy(uic,vic,wic,vx,vy,vz)
+         rom_time=0.
+         icalld=1
+      endif
+
+      stime=dnekclock()
+
+      ad_step = istep
+      jfield=ifield
+      ifield=1
+
+      call rom_setup_v
+      call sets_diag
+
+      if (nio.eq.0) write (6,*) 'starting rom_step loop',ad_nsteps
+
+      ad_step = 1
+      do i=1,ad_nsteps
+         call rom_step_v
+         time=time+dt
+         ad_step=ad_step+1
+      enddo
+
+      call sett_diag
+
+      if (nio.eq.0) write (6,*) eest_diag(),ad_re,'error_estimate'
+
+      ifield=jfield
+
+      dtime=dnekclock()-stime
+      rom_time=rom_time+dtime
+
+      return
+      end
+c-----------------------------------------------------------------------
+      function csig_diag(g1,g2,g3,h1,h2,op)
 
       include 'SIZE'
 
@@ -17,7 +65,7 @@ c-----------------------------------------------------------------------
 
       mio=nio
       nio=-1
-      csig=vecprod(r1,r2,r3,r1,r2,r3)
+      csig_diag=vecprod(r1,r2,r3,r1,r2,r3)
       nio=mio
 
       return
@@ -38,11 +86,13 @@ c-----------------------------------------------------------------------
       call opcopy(u1,u2,u3,g1,g2,g3)
 
       if (op.eq.'H10') then
+         call exitti('H10 not yet implemented$',1)
+         ! does not converge
          call ophinv(r1,r2,r3,g1,g2,g3,h1,h2,tolh,nmxhi)
       else if (op.eq.'L2 ') then
          call opbinv1(r1,r2,r3,g1,g2,g3,1.)
       else
-         call exitti('did not provide supported operator$')
+         call exitti('did not provide supported operator$',1)
       endif
 
       call opcopy(g1,g2,g3,u1,u2,u3)
@@ -50,7 +100,7 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
-      subroutine sets ! set sigmas
+      subroutine sets_diag ! set sigmas
 
       include 'SIZE'
       include 'MOR'
@@ -69,18 +119,18 @@ c-----------------------------------------------------------------------
          call col3(g1,ub(1,j),bm1,n)
          call col3(g2,vb(1,j),bm1,n)
          if (ldim.eq.3) call col3(g3,wb(1,j),bm1,n)
-         sigb(j)=csig(g1,g2,g3,ones,zeros,ips)
+         sigb_diag(j)=csig_diag(g1,g2,g3,ones,zeros,ips)
 
          call axhelm(g1,ub(1,j),ones,zeros,1,1)
          call axhelm(g2,vb(1,j),ones,zeros,1,1)
          if (ldim.eq.3) call axhelm(g3,wb(1,j),ones,zeros,1,1)
-         siga(j)=csig(g1,g2,g3,ones,zeros,ipsp)
+         siga_diag(j)=csig_diag(g1,g2,g3,ones,zeros,ips)
 
          call setcnv_c(ub(1,j),vb(1,j),wb(1,j))
          do i=0,nb
             call setcnv_u(ub(1,i),vb(1,i),wb(1,i))
             call ccu(g1,g2,g3)
-            sigc(i,j)=csig(g1,g2,g3,ones,zeros,ipsp)
+            sigc_diag(i,j)=csig_diag(g1,g2,g3,ones,zeros,ips)
          enddo
       enddo
 
@@ -89,36 +139,36 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
-      subroutine sett() ! set thetas
+      subroutine sett_diag ! set thetas
 
       include 'SIZE'
       include 'MOR'
 
-      call rzero(thb,nb+1)
-      call add2s2(thb,uj(0,1),-5./6,nb+1)
-      call add2s2(thb,uj(0,2), 1./6,nb+1)
-      call add2s2(thb,uj(0,3),-1./3,nb+1)
-      call add2s2(thb,uj(0,4), 1./3,nb+1)
-      call add2s2(thb,uj(0,5),-7./6,nb+1)
-      call add2s2(thb,uj(0,6),11./6,nb+1)
+      call rzero(thb_diag,nb+1)
+      call add2s2(thb_diag,uj(0,1),-5./6,nb+1)
+      call add2s2(thb_diag,uj(0,2), 1./6,nb+1)
+      call add2s2(thb_diag,uj(0,3),-1./3,nb+1)
+      call add2s2(thb_diag,uj(0,4), 1./3,nb+1)
+      call add2s2(thb_diag,uj(0,5),-7./6,nb+1)
+      call add2s2(thb_diag,uj(0,6),11./6,nb+1)
 
       s=1./(ad_dt*ad_nsteps)
-      call cmult(thb,s,nb+1)
+      call cmult(thb_diag,s,nb+1)
 
       s=1./ad_re
-      call cmult2(tha,ua,s,nb+1)
+      call cmult2(tha_diag,ua,s,nb+1)
 
-      call copy(thc,u2a,(nb+1)**2)
-c     call chsign(thc,(nb+1)**2)
+      call copy(thc_diag,u2a,(nb+1)**2)
+c     call chsign(thc_diag,(nb+1)**2)
 
-      call add2s2(thc,u2j(0,0,2),-1.*rinstep,(nb+1)**2)
-      call add2s2(thc,u2j(0,0,5),-1.*rinstep,(nb+1)**2)
-      call add2s2(thc,u2j(0,0,6), 2.*rinstep,(nb+1)**2)
+      call add2s2(thc_diag,u2j(0,0,2),-1.*rinstep,(nb+1)**2)
+      call add2s2(thc_diag,u2j(0,0,5),-1.*rinstep,(nb+1)**2)
+      call add2s2(thc_diag,u2j(0,0,6), 2.*rinstep,(nb+1)**2)
 
       return
       end
 c-----------------------------------------------------------------------
-      function eest() ! compute error estimate
+      function eest_diag() ! compute error estimate
 
       include 'SIZE'
       include 'MOR'
@@ -126,14 +176,14 @@ c-----------------------------------------------------------------------
       eest=0.
 
       do j=0,nb
-         eest=eest+sigb(j)*thb(j)**2
-         eest=eest+siga(j)*tha(j)**2
+         eest=eest+sigb_diag(j)*thb_diag(j)**2
+         eest=eest+siga_diag(j)*tha_diag(j)**2
          do i=0,nb
-            eest=eest+sigc(i,j)*thc(i,j)**2
+            eest=eest+sigc_diag(i,j)*thc_diag(i,j)**2
          enddo
       enddo
 
-      eest=sqrt(eest)
+      eest_diag=sqrt(eest)
 
       return
       end
