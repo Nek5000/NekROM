a38a26197d20667518be286822100dd5082b7fad
Kento Kaneko <kaneko2@illinois.edu>
2019-04-30 15:58:18 -0500

    run integ.sh if not unit

--- tests/test.sh
+++ tests/test.sh
@@ -14,9 +14,5 @@ $MOR_DIR/bin/gsnaps cyl_rect
 if [ ${TEST: -4} == "UNIT" ]; then
     bash $MOR_DIR/tests/unit.sh $TEST
 else
-    case "$TEST" in
-        BAF_INTEG)
-            $MOR_DIR/tests/baf-test
-            ;;
-    esac
+    bash $MOR_DIR/tests/integ.sh $TEST
 fi
