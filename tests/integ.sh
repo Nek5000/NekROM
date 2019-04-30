00d4f7848544a3ba6936e9f07d2ce580ceb10c32
Kento Kaneko <kaneko2@illinois.edu>
2019-04-30 15:56:50 -0500

    add integ.sh

---
+++ tests/integ.sh
@@ -0,0 +1,29 @@
+if [[ $1 =~ _L2_ ]]; then
+    ifl2='.TRUE.'
+    $MOR_DIR/bin/gops cyl_rect_l2
+    $MOR_DIR/bin/gbas cyl_rect_l2
+else
+    ifl2='.FALSE.'
+    $MOR_DIR/bin/gops cyl_rect_h10
+    $MOR_DIR/bin/gbas cyl_rect_h10
+fi
+
+mkdir ops
+
+name="$(echo $1 | perl -pe 's/_(L2|H10)_/_/g')(${ifl2})"
+$MOR_DIR/tests/test_template.sh 'call rom_update'
+
+ls ../../data/cyl_rect/cyl0.f* > file.list
+ls ../../data/cyl_rect/avgcyl0.f* > avg.list
+ls bas/bascyl0.f* > bas.list
+
+cp ../../data/cyl_rect/cyl0.f01000 r0.f00001
+
+$SOURCE_ROOT/bin/makenek test
+
+$SOURCE_ROOT/bin/genmap << Z
+test
+.01
+Z
+
+./nek5000
