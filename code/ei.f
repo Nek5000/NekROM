9a6c93bbf56fecd85d8cb39fbdea21ed0fa55ebe
Kento Kaneko <kaneko2@illinois.edu>
2019-05-19 11:22:51 -0500

    add heat support

--- code/ei.f
+++ code/ei.f
@@ -1,9 +1,16 @@
 c-----------------------------------------------------------------------
       subroutine cres(res,sigma,theta,n,l)
 
+      include 'SIZE'
+      include 'MOR'
+
       real sigma(l,l),theta(l)
 
-      call set_theta
+      if (eqn.eq.'POI')
+         call set_theta_poisson
+      else if (eqn.eq.'HEA')
+         call set_theta_heat
+      endif
 
       res=0.
 
@@ -20,7 +27,7 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
-      subroutine set_xi
+      subroutine set_xi_poisson
 
       include 'SIZE'
       include 'TOTAL'
@@ -54,6 +61,51 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
+      subroutine set_xi_heat
+
+      include 'SIZE'
+      include 'TOTAL'
+      include 'MOR'
+
+      parameter (lt=lx1*ly1*lz1*lelt)
+
+      common /poisson/ bqr(lx1*ly1*lz1*lelt)
+      common /eires/ xi(lt,lres),theta(lt,lres),sigma(lres,lres)
+      common /eiivar/ nres
+
+      n=lx1*ly1*lz1*nelv
+
+      l=1
+      if (ifield.eq.1) then
+         call exitti('(set_ritz_a) ifield.eq.1 not supported...$',nb)
+      else
+         if (ips.eq.'L2 ') then
+            do i=0,nb
+               call copy(xi(l,i),tb(1,i),n)
+               l=l+1
+            enddo
+            do i=0,nb
+               call axhelm(xi(l,i),tb(1,i),ones,zeros,1,1)
+               call binv1(xi(l,i))
+               l=l+1
+            enddo
+            call copy(xi(l,nb+2),bqr,n)
+            call binv1(xi(l,nb+2))
+            l=l+1
+         else
+            do i=0,nb
+               call copy(xi(1,i),tb(1,i),n)
+            enddo
+         endif
+      endif
+
+      if ((l-1).gt.nres) then
+         call exitti('increase nres$',l-1)
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
       subroutine set_sigma
 
       include 'SIZE'
@@ -69,7 +121,11 @@ c-----------------------------------------------------------------------
       if (nres.gt.lres) call exitti('nres > lres$',nres)
       if (nres.le.0) call exitti('nres <= 0$',nres)
 
-      call set_xi
+      if (eqn.eq.'POI') then
+         call set_xi_poisson
+      else if (eqn.eq.'HEA') then
+         call set_xi_heat
+      endif
 
       do i=1,nres
       do j=1,nres
@@ -80,7 +136,7 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
-      subroutine set_theta
+      subroutine set_theta_poisson
 
       include 'SIZE'
       include 'TOTAL'
@@ -97,6 +153,33 @@ c-----------------------------------------------------------------------
       enddo
 
       theta(nb+1)=-1.
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine set_theta_heat
+
+      include 'SIZE'
+      include 'TOTAL'
+      include 'MOR'
+
+      parameter (lt=lx1*ly1*lz1*lelt)
+      common /eires/ xi(lt,lres),theta(lres),sigma(lres,lres),betaj(6)
+      common /eiivar/ nres
+
+      n=lx1*ly1*lz1*nelv
+
+      l=1
+      call set_betaj(betaj)
+      call mxm(uj,lub+1,betaj,6,theta(l),1)
+      l=l+lub+1
+      do i=0,nb
+         theta(l)=uta(i)
+         l=l+1
+      enddo
+
+      theta(nb+1)=-1.
+      l=l+1
 
       return
       end
@@ -127,6 +210,8 @@ c     Matrices and vectors for advance
       last_time = dnekclock()
 
       n=lx1*ly1*lz1*nelt
+
+      eqn='POI'
 
       ifflow=.false.
       ifheat=.true.
@@ -180,6 +265,30 @@ c-----------------------------------------------------------------------
       do i=1,nb
          rhs(i)=glsc2(bqr,tb(1,i),n)
       enddo
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine set_betaj(betaj)
+
+      include 'SIZE'
+      include 'MOR'
+
+      betaj(6)
+
+      ! ad_beta(4,3)
+
+      betaj(1)=ad_beta(1+1,1)+ad_beta(2+1,2)+ad_beta(3+1,3)
+      betaj(2)=ad_beta(0+1,1)+ad_beta(1+1,2)+ad_beta(2+1,3)
+              +ad_beta(3+1,3)
+      betaj(3)=ad_beta(0+1,2)+ad_beta(1+1,3)+ad_beta(2+1,3)
+              +ad_beta(3+1,3)
+
+      betaj(4)=ad_beta(0+1,3)+ad_beta(1+1,3)+ad_beta(2+1,3)
+      betaj(5)=ad_beta(0+1,3)+ad_beta(1+1,3)
+      betaj(6)=ad_beta(0+1,3)
+
+      call cmult(betaj,1./(ad_dt*ad_nsteps),6)
 
       return
       end
