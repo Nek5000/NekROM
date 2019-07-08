36c2779c5c3b3911e4dbc80fd22735484d99214b
Kento Kaneko <kaneko2@illinois.edu>
2019-07-08 02:15:59 +0200

    move code outside

--- code/rom.f
+++ code/rom.f
@@ -763,10 +763,8 @@ c-----------------------------------------------------------------------
 
       if (ifdumpops) then
          call dump_serial(u,nb+1,'ops/uf ',nid)
-         call dump_serial(ua,nb+1,'ops/ua ',nid)
          if (ifrom(2)) then 
             call dump_serial(ut,nb+1,'ops/tf ',nid)
-            call dump_serial(uta,nb+1,'ops/uta ',nid)
          endif
          do i=0,nb
             t1(i)=u2a(i,i)-ua(i)*ua(i)
@@ -775,6 +773,9 @@ c-----------------------------------------------------------------------
          call dump_serial(uas,nb+1,'ops/uas ',nid)
          call dump_serial(uvs,nb+1,'ops/uvs ',nid)
       endif
+
+      call dump_serial(ua,nb+1,'ops/ua ',nid)
+      if (ifrom(2)) call dump_serial(uta,nb+1,'ops/uta ',nid)
 
       call dump_sfld
 
