d18c8658d76beef78b7c1f7f0c3af9bfb010af63
Kento Kaneko <kaneko2@illinois.edu>
2019-08-20 14:47:07 -0500

    save results to test.log

--- tests/integ.sh
+++ tests/integ.sh
@@ -15,6 +15,6 @@ Z
 sed -i.bu "s/lb=.*\)/lb=$LB)/g" LMOR
 sed -i.bu "s/^.*p177.*\$/$NB p177/g" LMOR
 
-mpiexec -np $NP ./nek5000 | tee test.log.$NP
+mpiexec -np $NP ./nek5000 | tee test.log
 
 ../$SCR
