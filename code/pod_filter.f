ec93bb98281b0196b6528b709d3041106e1e3547
Sean <r04246004@ntu.edu.tw>
2019-08-24 17:07:03 -0500

    add subroutine that supprot differential filter

--- code/pod_filter.f
+++ code/pod_filter.f
@@ -25,8 +25,43 @@ c     enddo
       return
       end
 c-----------------------------------------------------------------------
-      subroutine pod_df(uu,delta)
+      subroutine pod_df(uu)
 
+      include 'SIZE'
+      include 'TOTAL'
+      include 'MOR'
+
+      integer icalld
+      save    icalld
+      data    icalld /0/
+
+      real dfops(nb,nb)
+      real tmp(nb)
+
+      if (icalld.eq.0) then
+         call setdf(dfops,au,bu,rdft*rdft)
+         call dgetrf(nb,nb,dfops,lub,ipiv,info)
+      endif
+
+      call mxm(bu,nb,uu,nb,tmp,1)
+      call dgetrs('N',nb,1,dfops,lub,ipiv,tmp,nb,info)
+      call copy(uu,tmp,nb)
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine setdf(flu,a,b,ad_diff)
+
+      include 'SIZE'
+      include 'MOR'
+
+
+      real flu(nb,nb),a(nb,nb),b(nb,nb)
+      real ad_diff
+
+      call cmult2(flu,a,ad_diff,nb*nb)
+      call add2(flu,b,nb*nb)
+         
       return
       end
 c-----------------------------------------------------------------------
