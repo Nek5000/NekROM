fe4e51d500e5cd632d1026a31083469ca1105657
Sean <r04246004@ntu.edu.tw>
2019-08-30 09:39:38 -0500

    clean up POD proj

--- code/pod_filter.f
+++ code/pod_filter.f
@@ -8,19 +8,7 @@ c-----------------------------------------------------------------------
       real uu(nb)
       integer r1
 
-c     if (nid.eq.0) write(6,*)'r1:',r1,'nb-r1',nb-r1
-
-c     write(6,*)'quick check before'
-c     do ii=1,nb
-c     write(6,*)ii,uu(ii),u(ii,1)
-c     enddo
-
-      call cfill(uu(r1+1),0.0,nb-r1)
-
-c     write(6,*)'quick check after'
-c     do ii=1,nb
-c     write(6,*)ii,uu(ii),u(ii,1)
-c     enddo
+      call cfill(uu((nb-r1+1):nb),0.0,r1)
 
       return
       end
