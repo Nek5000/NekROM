80890ae0de3a3f5dbc1c32446602d1e80733580e
Kento Kaneko <kaneko2@illinois.edu>
2019-10-22 10:00:10 -0500

    add comment

--- code/rom.f
+++ code/rom.f
@@ -177,7 +177,7 @@ c-----------------------------------------------------------------------
       call setqoi
       call setmisc
       if (ifei) call set_sigma
-      ntr=nb
+      ntr=nb!-1
       if (ntr.gt.0) call set_trace
 
       if (nio.eq.0) write (6,*) 'end range setup'
