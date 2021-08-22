b8796d62cdfe4b0d54827d9e34cebaede1a8931e
Kento Kaneko <kaneko2@illinois.edu>
2021-08-22 18:04:34 -0500

    remove redundant nskip declaration

--- code/rom.f
+++ code/rom.f
@@ -224,8 +224,6 @@ c-----------------------------------------------------------------------
       call mor_show_params
       if (nid.eq.0) write (6,*) 'post show params'
 
-      nskip=0
-
       call mor_set_params_uni_pre
       call mor_init_fields
       call mor_set_params_uni_post
