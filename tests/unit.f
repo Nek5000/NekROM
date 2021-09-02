41e4b9f0eaf41f70db0c7178c4c983744ce0925c
Kento Kaneko <kaneko2@illinois.edu>
2021-09-02 14:07:27 -0500

    fix reference to ug in unit.f

--- tests/unit.f
+++ tests/unit.f
@@ -30,6 +30,8 @@ c-----------------------------------------------------------------------
       param(173) = 0.
 
       call rom_setup
+      call gengram(ug,us0,ns,ldim,ips)
+
       call read_serial(vv,ls*ls,'tops/gu ',wk,nid)
 
       iexit=0
@@ -42,8 +44,8 @@ c-----------------------------------------------------------------------
 
       do j=1,ls
       do i=1,ls
-         s1=s1+(ug(i,j,1)-ug(j,i,1))**2
-         s2=s2+(ug(i,j,1)-vv(i,j))**2
+         s1=s1+(ug(i,j)-ug(j,i))**2
+         s2=s2+(ug(i,j)-vv(i,j))**2
          s3=s3+vv(i,j)**2
          if (nio.eq.0) write (6,*) 'gram',i,j,ug(i,j,1),vv(i,j)
       enddo
