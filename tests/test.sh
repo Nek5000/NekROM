465dde6c55dec494849ed7015a2100f7bf6d4d55
Sean <r04246004@ntu.edu.tw>
2021-09-15 11:17:17 -0500

    use correct tcp when IPS=L2

--- tests/test.sh
+++ tests/test.sh
@@ -15,9 +15,15 @@ fold_start gsnaps "Get Snapshots"; $MOR_DIR/bin/gsnaps cyl_rect; fold_end gsnaps
 if [ "$IPS" = "L2" ]; then
     fold_start gops "Get operators"; $MOR_DIR/bin/gops cyl_rect_l2; fold_end gops
     fold_start gbas "Get basis";     $MOR_DIR/bin/gbas cyl_rect_l2; fold_end gbas
+   if [ "$TEST" = "CP" ]; then
+       fold_start gcp "Get cp operators"; $MOR_DIR/bin/gcp cyl_rect_l2; fold_end gcp
+   fi
 elif [ "$IPS" = "H10" ]; then
     fold_start gops "Get operators"; $MOR_DIR/bin/gops cyl_rect_h10; fold_end gops
     fold_start gbas "Get basis";     $MOR_DIR/bin/gbas cyl_rect_h10; fold_end gbas
+   if [ "$TEST" = "CP" ]; then
+       fold_start gcp "Get cp operators"; $MOR_DIR/bin/gcp cyl_rect_h10; fold_end gcp
+   fi
 else
     echo "inner-product space $IPS not supported..."
     exit 1
@@ -26,10 +32,6 @@ fi
 mv ops tops
 mkdir ops &> /dev/null
 
-echo $TEST
-if [ "$TEST" = "CP" ]; then
-    fold_start gcp "Get cp operators"; $MOR_DIR/bin/gcp cyl_rect_h10; fold_end gcp
-fi
 
 if [ "$TYPE" = "UNIT" ]; then
     bash $MOR_DIR/tests/unit.sh
