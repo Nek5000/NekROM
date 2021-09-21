0cfbf0d78feb0cccb4a85eb6e0558adec0a09908
Sean <r04246004@ntu.edu.tw>
2021-09-20 22:36:56 -0500

    add test for cp skew algorithm

--- tests/test.sh
+++ tests/test.sh
@@ -20,12 +20,16 @@ if [ "$IPS" = "L2" ]; then
     fold_start gbas "Get basis";     $MOR_DIR/bin/gbas cyl_rect_l2; fold_end gbas
    if [ "$TEST" = "CP" ]; then
        fold_start gcp "Get cp operators"; $MOR_DIR/bin/gcp cyl_rect_l2; fold_end gcp
+   elif [ "$TEST" = "CP_SKEW" ]; then
+       fold_start gcp "Get cp operators"; $MOR_DIR/bin/gcp cyl_rect_l2_skew; fold_end gcp
    fi
 elif [ "$IPS" = "H10" ]; then
     fold_start gops "Get operators"; $MOR_DIR/bin/gops cyl_rect_h10; fold_end gops
     fold_start gbas "Get basis";     $MOR_DIR/bin/gbas cyl_rect_h10; fold_end gbas
    if [ "$TEST" = "CP" ]; then
        fold_start gcp "Get cp operators"; $MOR_DIR/bin/gcp cyl_rect_h10; fold_end gcp
+   elif [ "$TEST" = "CP_SKEW" ]; then
+       fold_start gcp "Get cp operators"; $MOR_DIR/bin/gcp cyl_rect_h10_skew; fold_end gcp
    fi
 else
     echo "inner-product space $IPS not supported..."
