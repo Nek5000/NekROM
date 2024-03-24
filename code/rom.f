552f5b5e121287aae7ac30a9fde7d423f50e34bf
Kento Kaneko <kaneko@mit.edu>
2024-03-24 12:42:11 -0400

    break line

--- code/rom.f
+++ code/rom.f
@@ -1345,7 +1345,8 @@ c                    call setcnv_u1(tb(1,j))
                enddo
                call breduce(rtmp1,nb,1)
                do i=1,nb
-                  call setc_local(cl,rtmp1(i,1),ic1,ic2,jc1,jc2,kc1,kc2,i,j,k)
+                  call setc_local(cl,rtmp1(i,1),
+     $               ic1,ic2,jc1,jc2,kc1,kc2,i,j,k)
                   if (nid.eq.0) write (100,*) cel
                enddo
             enddo
