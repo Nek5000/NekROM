a8a4c7e23a72f8453f8c0478e2a46688ecc9a866
Ping-Hsuan <pht2@illinois.edu>
2024-07-06 23:20:03 -0400

    remove ;;

--- tests/matlab_test/test.sh
+++ tests/matlab_test/test.sh
@@ -2,7 +2,7 @@ cd $MOR_DIR/tests
 source travis_fn
 mkdir t
 cd t
-gdown https://drive.google.com/uc?id=1-5ryuIACyWtOzSl3y0y0SYKBqIGofrO3;;
+gdown https://drive.google.com/uc?id=1-5ryuIACyWtOzSl3y0y0SYKBqIGofrO3
 unzip Re200.zip
 cd Re200/T20/
 cp $MOR_DIR/bin/matlab_driver/*.m .
