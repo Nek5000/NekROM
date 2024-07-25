64fa95d9990c4a037e2d3c2e6f59e805a2e4573d
Kento Kaneko <kaneko@mit.edu>
2024-07-25 12:01:39 -0400

    updated restart and post-processing to show reconstructed drag

--- examples/cyl/post.m
+++ examples/cyl/post.m
@@ -5,16 +5,24 @@ rx=dlmread('rom.dragx.dat');
 ry=dlmread('rom.dragy.dat');
 
 % plot fom result:
+
+tstart=401;
+tend=500;
+
 figure;
-plot(fx(:,2),fx(:,3)); xlim([400 500]);
-title('FOM drag result');
+plot(fx(:,2),fx(:,3)); xlim([tstart tend]);
+title('Drag result');
 xlabel('Time');
 ylabel('Drag');
 
 % plot rom result:
-figure;
-plot(rx(:,1),rx(:,4)); xlim([400 500]);
-title('ROM drag result');
+%figure;
+hold on
+rx(:,1)=rx(:,1)+tstart; % restart from t=400
+%plot(rx(:,1),rx(:,4)); xlim([400 500]);
+plot(rx(:,1),rx(:,4),'--');
+legend('FOM','ROM');
+%title('ROM drag result');
 xlabel('Time');
 ylabel('Drag');
 
