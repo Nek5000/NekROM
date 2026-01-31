be19adb4ca7f6b910cdf04ebd155e19e002680dd
Nicholas Christensen <njchris2@illinois.edu>
2026-01-30 19:35:56 -0600

    Improvements to the two important point selection algorithms

--- drive/matlab/point_generators/gpode.m
+++ drive/matlab/point_generators/gpode.m
@@ -1,17 +1,52 @@
-function[p] = gpode (U, m)
- [~ , ~ , p] = qr (U' , 'vector');
- p = p(1:size(U, 2))';
- for i = (length(p) + 1):m
-   [~ , S , W] = svd(U(p, :) , 0);
-   g = S(end - 1, end - 1).^2 - S(end, end)^2;
-   Ub = W'* U';
-   r = g + sum(Ub.^2 , 1);
-   r = r - sqrt((g + sum (Ub.^2 ,1)).^2 - 4*g*Ub(end, :).^2);
-   [~ , I] = sort(r , 'descend');
-   e = 1;
-   while any(I(e) == p)
-     e = e + 1;
-   end
-   p(end + 1) = I(e);
- end
+% The original gpode code is in `extra` folder.
+% This is the result after querying Gemini for
+% improvements.
+function p = gpode(U, m)
+    [n_samples, n_vars] = size(U);
+    p = zeros(m, 1);
+    
+    % Initial selection via QR pivoting
+    [~, ~, p_init] = qr(U', 'vector');
+    p(1:n_vars) = p_init(1:n_vars);
+    
+    % Pre-transpose for column-major efficiency
+    Ut = U'; 
+    
+    % Initial decomposition of the first n_vars rows
+    % We maintain the SVD of the currently selected subset
+    [~, S, W] = svd(U(p(1:n_vars), :), 0);
+    
+    for i = (n_vars + 1):m
+        % 1. Compute the critical values from the current SVD
+        s_prev = S(end-1, end-1)^2;
+        s_curr = S(end, end)^2;
+        g = s_prev - s_curr;
+        
+        % 2. Project all candidate rows onto the current basis W
+        Ub = W' * Ut; % Efficient projection
+        Ub_sq = sum(Ub.^2, 1);
+        
+        % 3. Calculate the objective function 'r'
+        term1 = g + Ub_sq;
+        r = term1 - sqrt(max(0, term1.^2 - 4 * g * Ub(end, :).^2));
+        
+        % 4. Find the best new index (avoiding duplicates)
+        [~, I] = sort(r, 'descend');
+        idx = 1;
+        while any(p(1:i-1) == I(idx))
+            idx = idx + 1;
+        end
+        new_p = I(idx);
+        p(i) = new_p;
+        
+        % 5. INCREMENTAL UPDATE: Update S and W for the next iteration
+        % We append the new row u_new = U(new_p, :) to our set.
+        % This is a rank-1 update to the Gramian or a row-append to the SVD.
+        u_new = U(new_p, :);
+        
+        % We use a standard SVD "row-addition" update logic:
+        % Update W and S by performing SVD on the small [S; u_new * W] matrix
+        [~, S, W_new] = svd([S; u_new * W], 0);
+        W = W * W_new; % Rotate the basis
+    end
 end
--- drive/matlab/point_generators/s_opt.m
+++ drive/matlab/point_generators/s_opt.m
@@ -1,81 +1,98 @@
-function [ index ] = s_opt( Vo, M, index, outfile )
-% Generates S-optimal indices of a matrix
+function [index] = s_opt(Vo, M, index, outfile)
+% S_OPT Generates S-optimal indices of a matrix
+%   Optimized implementation focusing on vectorization and memory efficiency.
+%   Obtained by querying Gemini for improvements to the original algorithm.
 %
-% Parameters: 
-%   Vo:      Candidate Matrix
-%   M:       The number of S-optimal points to be computed, number of rows basically
-%   idx:     If there is a previous index set, put it in. `If not, just put [].
-%   outfile: Put 'filename', then 'filename' will be created with the information of index.
-% 
-% Returns:    
-%   S-optimal points index set, of size (M x 1)
-%
-% This implementation is adapted from 
-% `this version <https://github.com/LLNL/libROM/blob/d8cc32ce02f981fd5bf12ab32ef8d4c3445eab18/unit_tests/matlab/s_opt_generator.m>`_
-% written by Yeonjong Shin for LibROM.
-%
-[N_Bm, N] = size(Vo);
-nVo = Vo.*Vo;
+%   Parameters:
+%       Vo      : Candidate Matrix (N_Bm x N)
+%       M       : Number of S-optimal points to compute
+%       index   : Initial index set (use [] if none)
+%       outfile : Optional filename string for saving results
 
-inum = length(index);
-if  inum == 0
-    [~, i_idx] = max(abs(Vo(:,1)));
-    index = [];
-    index = [index; i_idx];
-    inum = inum + 1;
-end
+    [N_Bm, N] = size(Vo);
+    nVo = Vo.^2; % Pre-compute squared elements
 
-for i = inum+1 : M
-    if i < N+1
-        V1 = Vo(index, 1:i);
-        A0 = V1(1:i-1,1:i-1); atA0 = V1(1:i-1,i)'*A0;
-        tt = zeros(N_Bm,i-1); tt1 = zeros(i-1,N_Bm);
-        ata = sum(V1(1:i-1,i).^2) ; 
-        bbb = (A0'*A0)\[atA0; Vo(:,1:i-1)]'; c = bbb(:,2:end);
-        b = 1 + sum(Vo(:,1:i-1).*c',2);
-        
-        for zz = 1 : i-1
-            tt(:,zz) = Vo(:,zz).*Vo(:,i);
-        end
-        g1 = repmat(atA0,N_Bm,1) + tt; g1 = g1';
-        g2 = bbb(:,1);
-        oneprc = 1 + sum(Vo(:,1:i-1).*c',2);
-        g3 = sum(c'.*g1',2)./oneprc;
-        for zz = 1 : i-1
-            tt1(zz,:) = c(zz,:).*(Vo(:,i) - g3)';
-        end
-        
-        GG = repmat(g2,1,N_Bm) + tt1; 
-
-        A = ata + Vo(:,i).^2 - sum(g1'.*GG',2);
-        A(A<0) = 0;
-        nV = sum(nVo(index,1:i),1);
-        noM = sum(log(repmat(nV,N_Bm,1) + nVo(:,1:i)),2);        
-        
-        A = log(abs(A)) + log(b) - noM;
+    % Pre-allocate the full index vector
+    final_index = zeros(M, 1);
+    inum = length(index);
+    if inum > 0
+        final_index(1:inum) = index;
     else
-        V1 = Vo(index, :);
-        b = (V1'*V1)\Vo';
-        nV = sum(nVo(index,:));  
-        noM = sum(log(repmat(nV,N_Bm,1) + nVo),2);
-        A = log(1+sum(Vo.*b',2)) - noM;
+        % Initial step: Maximize the magnitude of the first basis vector
+        [~, i_idx] = max(abs(Vo(:, 1)));
+        final_index(1) = i_idx;
+        inum = 1;
     end
-    A(index) = -inf;
-    [~, ggg] = max(A);
-    index = [index; ggg(1)];
 
-    if i == floor(M/4) || i == floor(M/2) || i == floor(3/4*M)
-        fprintf('i = %d, index = %d, N = %d, M = %d\n', i, ggg(1), N, M);
-    elseif i == M
-        fprintf('Done\n');
+    % Main optimization loop
+    for i = inum + 1 : M
+        curr_idx = final_index(1:i-1);
+        
+        if i <= N
+            % --- Under-determined or Exact Case ---
+            % Current basis subset
+            V_prev = Vo(curr_idx, 1:i-1);
+            V_curr_col = Vo(curr_idx, i);
+            
+            % Compute intermediate terms
+            atA0 = V_curr_col' * V_prev;
+            ata = sum(V_curr_col.^2);
+            
+            % FIXED: Corrected vertical concatenation [atA0; Vo']
+            % bbb results in (i-1) x (1 + N_Bm)
+            bbb = (V_prev' * V_prev) \ [atA0; Vo(:, 1:i-1)]';
+            
+            c = bbb(:, 2:end); % (i-1) x N_Bm
+            g2 = bbb(:, 1);    % (i-1) x 1
+            
+            % Vectorized calculation of b (N_Bm x 1)
+            % Vo(:, 1:i-1) is (N_Bm x i-1), c' is (N_Bm x i-1)
+            b = 1 + sum(Vo(:, 1:i-1) .* c', 2);
+            
+            % Vectorized calculation of g1 ((i-1) x N_Bm)
+            % Implicit expansion: (1 x i-1) + (N_Bm x i-1) -> (N_Bm x i-1)
+            g1 = (atA0 + (Vo(:, 1:i-1) .* Vo(:, i)))';
+            
+            % Vectorized calculation of g3 and GG
+            g3 = sum(c' .* g1', 2) ./ b;
+            GG = g2 + (c .* (Vo(:, i) - g3)'); 
+            
+            % Compute selection criterion A
+            A_val = ata + Vo(:, i).^2 - sum(g1' .* GG', 2);
+            A_val(A_val < 0) = 0;
+            
+            % Implicit expansion for log-determinant terms
+            nV = sum(nVo(curr_idx, 1:i), 1);
+            noM = sum(log(nV + nVo(:, 1:i)), 2);
+            
+            A = log(abs(A_val) + eps) + log(b) - noM;
+            
+        else
+            % --- Over-determined Case ---
+            V_sub = Vo(curr_idx, :);
+            b_coeff = (V_sub' * V_sub) \ Vo';
+            
+            nV = sum(nVo(curr_idx, :), 1);
+            noM = sum(log(nV + nVo), 2);
+            
+            A = log(1 + sum(Vo .* b_coeff', 2)) - noM;
+        end
+        
+        % Avoid selecting existing points
+        A(curr_idx) = -inf;
+        
+        % Select the next optimal index
+        [~, best_idx] = max(A);
+        final_index(i) = best_idx(1);
+
+        % Progress reporting
+        if ismember(i, floor(M * [0.25, 0.5, 0.75]))
+            fprintf('Progress: %d/%d points computed...\n', i, M);
+        end
     end
     
+    index = final_index;
+    if nargin > 3 && ~isempty(outfile)
+        save([outfile, '.mat'], 'index');
+    end
 end
-
-if exist("outfile") == 1
-    sfile = sprintf('%s%s',outfile,'.mat');
-    save(sfile,'index');
-end
-    
-end
-
