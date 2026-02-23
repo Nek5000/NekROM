48a94876f667b537230cd2ac2e1cdf803f476cda
Nicholas Christensen <njchris2@illinois.edu>
2026-02-23 17:37:55 -0600

    Add Octave-compatible path

--- drive/matlab/operators/gen_Au.m
+++ drive/matlab/operators/gen_Au.m
@@ -1,46 +1,56 @@
-function[Au, Bu, u0] = gen_Au(pod_u, pod_v, x, y)
-
-    % ROM diffusion operator.
-    % This should have the same action as the Au operator from the Fortran code
-    % Should probably add convection tensor calculation as well.
-
-    %x=snaps.flds{1}.x;
-    %y=snaps.flds{1}.y;
-    nx1 = size(x,1);
-    [zi, w] = zwgll(nx1-1);
+function [Au, Bu] = gen_Au(pod_u, pod_v, x, y)
+    % ROM diffusion operator - Hybrid MATLAB/Octave Implementation
+    
+    [nx, ny] = size(x);
+    nL = nx * ny;
+    nPOD = size(pod_u, 2);
+    
+    % 1. Setup Operators & Geometry
+    [zi, w] = zwgll(nx-1);
     Dh = deriv_mat(zi);
     Dht = Dh';
-    [xr,yr,xs,ys,rx,ry,sx,sy,jac,jaci,d] = deriv_geo(x,y,Dh);
-    nL = prod(size(x));
-    % The rx etc. arrays aren't multiplied by jaci. Assigning Me = massmatrix / Jac does the same thing.
-    Me = jaci.*(w*w');
+    W = w * w'; 
+    [~,~,~,~,rx,ry,sx,sy,jac,jaci,~] = deriv_geo(x, y, Dh);
+    
+    % 2. Calculate Geometric Factors
+    Me_inv = jaci .* W;
+    Grr = reshape(Me_inv .* (rx.^2 + ry.^2), nL, 1);
+    Grs = reshape(Me_inv .* (rx.*sx + ry.*sy), nL, 1);
+    Gss = reshape(Me_inv .* (sx.*sx + sy.*sy), nL, 1);
 
-    % Calculate geometric factors
-    Grr = reshape(Me.*(rx.*rx + ry.*ry),nL,1);
-    Grs = reshape(Me.*(rx.*sx + ry.*sy),nL,1);
-    Gss = reshape(Me.*(sx.*sx + sy.*sy),nL,1);
+    % 3. Branching Logic for Differentiation
+    if exist('pagemtimes', 'builtin') || exist('pagemtimes', 'file')
+        % --- MATLAB Optimized Path ---
+        u_3d = reshape(pod_u, nx, ny, nPOD);
+        v_3d = reshape(pod_v, nx, ny, nPOD);
+        
+        ur = reshape(pagemtimes(Dh, u_3d), nL, nPOD);
+        us = reshape(pagemtimes(u_3d, Dht), nL, nPOD);
+        vr = reshape(pagemtimes(Dh, v_3d), nL, nPOD);
+        vs = reshape(pagemtimes(v_3d, Dht), nL, nPOD);
+    else
+        % --- Octave / Legacy Path ---
+        % d/dr (Left derivative) is vectorized via matrix stacking
+        ur = reshape(Dh * reshape(pod_u, nx, ny * nPOD), nL, nPOD);
+        vr = reshape(Dh * reshape(pod_v, nx, ny * nPOD), nL, nPOD);
+        
+        % d/ds (Right derivative) requires a loop for the column-wise mult
+        us = zeros(nL, nPOD); 
+        vs = zeros(nL, nPOD);
+        for i = 1:nPOD
+            u_m = reshape(pod_u(:,i), nx, ny);
+            v_m = reshape(pod_v(:,i), nx, ny);
+            us(:,i) = reshape(u_m * Dht, nL, 1);
+            vs(:,i) = reshape(v_m * Dht, nL, 1);
+        end
+    end
 
-    % Kento's ROM approach. Calculate the derivatives of the POD modes
-    ur_pods = [];
-    us_pods = [];
-    vr_pods = [];
-    vs_pods = [];
-    for i = 1:size(pod_u,2);
-        pod_u_vec = reshape(pod_u(:,i),size(x));
-        pod_v_vec = reshape(pod_v(:,i),size(x));
-        ur_pods = [ur_pods, reshape(pagemtimes(Dh,pod_u_vec), nL,1)];
-        us_pods = [us_pods, reshape(pagemtimes(pod_u_vec,Dht), nL,1)];
-        vr_pods = [vr_pods, reshape(pagemtimes(Dh,pod_v_vec), nL,1)];
-        vs_pods = [vs_pods, reshape(pagemtimes(pod_v_vec,Dht), nL,1)];
-        %ur_pods = [ur_pods, reshape(tensorprod(Dh, pod_u_vec, [2],[1]), nL,1)];
-        %us_pods = [us_pods, reshape(tensorprod(pod_u_vec, Dht,[2],[1]), nL,1)];
-        %vr_pods = [vr_pods, reshape(tensorprod(Dh, pod_v_vec, [2],[1]), nL,1)];
-        %vs_pods = [vs_pods, reshape(tensorprod(pod_v_vec, Dht,[2],[1]), nL,1)];
+    % 4. Build Au (Symmetric Diffusion Operator)
+    % Grouping u and v terms for numerical stability and clarity
+    Au = (ur' * (Grr .* ur + Grs .* us) + us' * (Grs .* ur + Gss .* us)) + ...
+         (vr' * (Grr .* vr + Grs .* vs) + vs' * (Grs .* vr + Gss .* vs));
 
-    end;
-    Au = [ur_pods;us_pods]'* ([Grr.*ur_pods + Grs.*us_pods; Grs.*ur_pods + Gss.*us_pods]) + [vr_pods;vs_pods]'* ([Grr.*vr_pods + Grs.*vs_pods; Grs.*vr_pods + Gss.*vs_pods]); 
-
-    Me = reshape(jac.*(w*w'),nL,1);
-    bas = [pod_u;pod_v];
-    Bu = bas'*(spdiags([Me;Me],0, 2*nL,2*nL)*bas); 
+    % 5. Build Bu (Mass Matrix)
+    Me_vec = reshape(jac .* W, nL, 1);
+    Bu = pod_u' * (Me_vec .* pod_u) + pod_v' * (Me_vec .* pod_v);
 end
