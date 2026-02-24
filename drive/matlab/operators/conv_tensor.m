b7449cbd0bf86cce7ec8fb2d64ad28b7bc095a0c
Nicholas Christensen <njchris2@illinois.edu>
2026-02-23 20:36:35 -0600

    Implement de-aliased convection tensor in matlab

--- drive/matlab/operators/conv_tensor.m
+++ drive/matlab/operators/conv_tensor.m
@@ -1,105 +1,107 @@
-function [out_coef] = conv_tensor(ucoef, pod_u, pod_v, x, y)
-
-    % Compute the full convection tensor
-    % Mostly replaced by conv_tensor_dense
-
-    persistent Me rx ry sx sy jaci d lgrad nL nb tensor
-    %persistent tensor
-
+function [out_coef, T_mat] = conv_tensor(ucoef, pod_u, pod_v, x, y)
+    % Optimized Convection Tensor for NekROM
+    % C(i,j,k) = < phi_i . grad(phi_j) , phi_k >
+    
+    persistent tensor nb n_test
+    
+    % --- Offline Stage (Pre-calculation) ---
     if isempty(tensor)
-        nx1 = size(x,1);
+        [nx1, ny1] = size(x);
+        nL = nx1 * ny1;
+        nb = size(pod_u, 2);
+        n_test = nb - 1; % Projection onto the POD modes 2:nb
+        
         [zi, w] = zwgll(nx1-1);
         d = deriv_mat(zi);
-        [xr,yr,xs,ys,rx,ry,sx,sy,jac,jaci,d] = deriv_geo(x,y,d);
-        lgrad=@(u,mode) grad(u,rx,ry,sx,sy,jaci,d,mode);
-        nL = prod(size(x));
-        nb = size(pod_u,2)
-        Me = reshape(jac.*(w*w'),nL,1);
-
-    if false; % Normal way of calculating the gradient
-        [ux_fom, uy_fom] = lgrad(u_fom, 0);
-        [vx_fom, vy_fom] = lgrad(v_fom, 0);
-    else
-        % Kento's ROM approach. Calculate the gradients of the POD modes
-        ux_pods = zeros(size(pod_u));
-        uy_pods = zeros(size(pod_u));
-        vx_pods = zeros(size(pod_u));
-        vy_pods = zeros(size(pod_u));
         
-        % For Pseudo-FOM version
-        %{
-        ux_fom = zeros(size(x));
-        uy_fom = zeros(size(x));
-        vx_fom = zeros(size(x));
-        vy_fom = zeros(size(x));
-        %}
-        for i = 1:size(pod_u,2);
-            [ux_pod, uy_pod] = lgrad(reshape(pod_u(:,i),size(x)),0);
-            [vx_pod, vy_pod] = lgrad(reshape(pod_v(:,i),size(x)),0);
-           
-            %{
-            % For Pseudo-FOM version 
-            ux_fom = ux_fom + ux_pod*ucoef(i);
-            uy_fom = uy_fom + uy_pod*ucoef(i);
-            vx_fom = vx_fom + vx_pod*ucoef(i);
-            vy_fom = vy_fom + vy_pod*ucoef(i);
-            %}
+        % Get geometric factors. jaci = 1/J, jac = J.
+        [~,~,~,~,rx,ry,sx,sy,jac,jaci,~] = deriv_geo(x, y, d);
+        
+        % Corrected weight: Me = J * w_i * w_j
+        Me = reshape(jac .* (w * w'), nL, 1);
+        
+        % Pre-allocate gradient storage
+        ux_p = zeros(nL, nb); uy_p = zeros(nL, nb);
+        vx_p = zeros(nL, nb); vy_p = zeros(nL, nb);
+        
+        % Calculate all gradients once
+        for i = 1:nb
+            ui = reshape(pod_u(:, i), size(x));
+            vi = reshape(pod_v(:, i), size(x));
+            
+            [ux, uy] = grad(ui, rx, ry, sx, sy, jaci, d, 0);
+            [vx, vy] = grad(vi, rx, ry, sx, sy, jaci, d, 0);
+            
+            ux_p(:, i) = ux(:); uy_p(:, i) = uy(:);
+            vx_p(:, i) = vx(:); vy_p(:, i) = vy(:);
+        end
 
-            ux_pods(:,i) = reshape(ux_pod, nL,1);
-            uy_pods(:,i) = reshape(uy_pod, nL,1);
-            vx_pods(:,i) = reshape(vx_pod, nL,1);
-            vy_pods(:,i) = reshape(vy_pod, nL,1);
-        end;
-        %ux_fom = reshape(ux_pods*ucoef, size(x));
-        %uy_fom = reshape(uy_pods*ucoef, size(x));
-        %vx_fom = reshape(vx_pods*ucoef, size(x));
-        %vy_fom = reshape(vy_pods*ucoef, size(x));
+        if false;        
+            % Performant version
+            % Pre-weight the test functions (modes 2 to nb)
+            % This combines the velocity components and mass matrix weights
+            phi_k_weighted = [Me .* pod_u(:, 2:nb); Me .* pod_v(:, 2:nb)];
+            
+            % Assemble 3D Tensor C(i,j,k)
+            % We iterate over i to keep memory overhead manageable
+            tensor = zeros(nb, nb, n_test);
+            for i = 1:nb
+                % Convection term for a fixed i across all j: (phi_i . grad(phi_j))
+                % Matrix size: (2*nL) x nb
+                conv_ij = [bsxfun(@times, pod_u(:,i), ux_p) + bsxfun(@times, pod_v(:,i), uy_p);
+                           bsxfun(@times, pod_u(:,i), vx_p) + bsxfun(@times, pod_v(:,i), vy_p)];
+                
+                % Project onto test space k: result is nb x n_test
+                % This uses matrix-matrix multiply (BLAS3)
+                tensor(i, :, :) = (conv_ij' * phi_k_weighted);
+            end
+            
+            % Permute for optimal online contraction: [k, i, j]
+            %tensor = permute(tensor, [3, 1, 2]);
+            tensor = permute(tensor, [3,2,1]);
 
-        %pod_u_weak = Me.*pod_u;
-        %pod_v_weak = Me.*pod_v;
+        else
+            % k: Test function index (The mode we project onto)
+            for k = 1:n_test
+                % Readable version
+                % Get the weighted test function (mode k+1)
+                % Reshaped for a dot product
+                wk_u = Me .* pod_u(:, k+1);
+                wk_v = Me .* pod_v(:, k+1);
+                
+                % i: Trial function index (The 'convecting' velocity)
+                for i = 1:nb
+                    ui = pod_u(:, i);
+                    vi = pod_v(:, i);
+                    
+                    % j: Basis function index (The 'convected' field)
+                    for j = 1:nb
+                        % Inner product: (phi_k) dot (phi_i * grad(phi_j))
+                        % C_kij = sum( wk * (ui*ux_j + vi*uy_j) )
+                        conv_x = ui .* ux_p(:, j) + vi .* uy_p(:, j);
+                        conv_y = ui .* vx_p(:, j) + vi .* vy_p(:, j);
 
-        % Could also apply Me to pod_u and pod_v inside the loop instead
-        % but this seems more efficient.
-        pod_weak = [Me.*pod_u(:,2:nb);Me.*pod_v(:,2:nb)];
-        tensor = zeros(nb,nb,nb-1);
-        for i=1:nb;
-            %pod_u_weak = Me.*pod_u(:,i);
-            %pod_v_weak = Me.*pod_v(:,i); 
-            %tensor(i,:,:) = [pod_u_weak.*ux_pods + pod_v_weak.*uy_pods;
-            %                 pod_u_weak.*vx_pods + pod_v_weak.*vy_pods]'*pod(:,2:nb);
-            tensor(i,:,:) = [pod_u(:,i).*ux_pods + pod_v(:,i).*uy_pods;
-                             pod_u(:,i).*vx_pods + pod_v(:,i).*vy_pods]'*pod_weak;
+                        %tensor(k, i, j) = wk_u' * conv_x + wk_v' * conv_y;
+                        tensor(k, j, i) = wk_u' * conv_x + wk_v' * conv_y;
+                    end
+                end
+            end
 
-            %{
-            % More readable version
-            for j=1:nb;
-                % Calculate pencil
-                [i,j]
-                tensor(i,j,:) = pod_weak'*[pod_u(:,i).*ux_pods(:,j) + pod_v(:,i).*uy_pods(:,j); 
-                                              pod_u(:,i).*vx_pods(:,j) + pod_v(:,i).*vy_pods(:,j)];
-                % Reduce pencil
-                %ijk = pod(:,2:nb)'*ij;
-                %tensor(i,j,:) = ijk;
-            end;
-            %}
         end
     end
-        
-    end; 
-    % End of if block to persist the tensor. Note, if comparing against the conv_fom approach
-    % this will need to be moved back up
+    
+    % --- Online Stage (Performance Critical) ---
+    % Goal: compute sum_{i,j} C(k,i,j) * u(i) * u(j)
+    % We use a reshaped matrix-vector product for O(N^2) speed.
+    
+    % Contract over j: temp(k, i) = C(k, i, :) * ucoef
+    T_mat = reshape(tensor, n_test * nb, nb);
+    temp = reshape(T_mat * ucoef, n_test, nb);
+   
+    %out_coef = temp*ucoef;
+    
+    % Contract over i: out(k) = temp(k, i) * ucoef
+    out_coef = sum(temp .* ucoef', 2);
 
-    %{
-    % For Pseudo-FOM version
-    u_fom = reshape(Me.*(pod_u*ucoef), size(x));
-    v_fom = reshape(Me.*(pod_v*ucoef), size(x));
-    conv_u_fom = reshape(u_fom.*ux_fom + v_fom.*uy_fom, nL,1);
-    conv_v_fom = reshape(u_fom.*vx_fom + v_fom.*vy_fom, nL,1);    
-    out_coef = [pod_u(:,2:end); pod_v(:,2:end)]'*[conv_u_fom; conv_v_fom]
-    %}     
-
-    outprod = tensorprod(tensor, ucoef, 1,1);
-    out_coef = tensorprod(outprod,ucoef,1,1);
-    %out_coef
-    %exit;
+    %out_coef = (reshape(tensor*ucoef(:,1),nb,nb+1)*ucoef(:,1));
 end
---
+++ drive/matlab/operators/conv_tensor_dealiased.m
@@ -0,0 +1,116 @@
+function [out_coef, T_mat] = conv_tensor(ucoef, pod_u, pod_v, x, y)
+    % Optimized De-aliased Convection Tensor for NekROM [N, N, E]
+    
+    persistent tensor nb n_test
+    
+    if isempty(tensor)
+        % --- Configuration ---
+        use_vectorized = true; % Set to true for performance, false for readability
+        
+        [nx1, ny1, n_elem] = size(x); 
+        nb = size(pod_u, 2);
+        n_test = nb - 1; 
+        
+        % 3/2 Rule for de-aliasing (e.g., 9 -> 14)
+        nx_f = ceil(1.5 * nx1); 
+        nL_f_total = nx_f * nx_f * n_elem;
+        
+        % --- Operators ---
+        [zi, ~] = zwgll(nx1-1);           
+        [zi_f, w_f] = zwgll(nx_f-1);      
+        d_f = deriv_mat(zi_f);            
+        Interp = interp_mat(zi_f, zi);    
+        
+        % --- Geometry & Interpolation ---
+        x_f = zeros(nx_f, nx_f, n_elem);
+        y_f = zeros(nx_f, nx_f, n_elem);
+        for ie = 1:n_elem
+            x_f(:,:,ie) = Interp * x(:,:,ie) * Interp';
+            y_f(:,:,ie) = Interp * y(:,:,ie) * Interp';
+        end
+        
+        [~,~,~,~,rx_f,ry_f,sx_f,sy_f,jac_f,jaci_f,~] = deriv_geo(x_f, y_f, d_f);
+        
+        % Fine weights: [nx_f*nx_f*n_elem, 1]
+        W2D = w_f * w_f';
+        Me_f = zeros(nx_f, nx_f, n_elem);
+        for ie = 1:n_elem
+            Me_f(:,:,ie) = jac_f(:,:,ie) .* W2D;
+        end
+        Me_f = Me_f(:); 
+        
+        % Interpolate POD to Fine Grid
+        pod_u_f = zeros(nL_f_total, nb);
+        pod_v_f = zeros(nL_f_total, nb);
+        for i = 1:nb
+            ui_c = reshape(pod_u(:, i), nx1, ny1, n_elem);
+            vi_c = reshape(pod_v(:, i), nx1, ny1, n_elem);
+            ui_f = zeros(nx_f, nx_f, n_elem);
+            vi_f = zeros(nx_f, nx_f, n_elem);
+            for ie = 1:n_elem
+                ui_f(:,:,ie) = Interp * ui_c(:,:,ie) * Interp';
+                vi_f(:,:,ie) = Interp * vi_c(:,:,ie) * Interp';
+            end
+            pod_u_f(:, i) = ui_f(:);
+            pod_v_f(:, i) = vi_f(:);
+        end
+        
+        % Batch Gradients
+        [ux_f_mat, uy_f_mat] = grad(reshape(pod_u_f, nx_f, nx_f, n_elem * nb), ...
+                                    repmat(rx_f, [1,1,nb]), repmat(ry_f, [1,1,nb]), ...
+                                    repmat(sx_f, [1,1,nb]), repmat(sy_f, [1,1,nb]), ...
+                                    repmat(jaci_f, [1,1,nb]), d_f, 0);
+        [vx_f_mat, vy_f_mat] = grad(reshape(pod_v_f, nx_f, nx_f, n_elem * nb), ...
+                                    repmat(rx_f, [1,1,nb]), repmat(ry_f, [1,1,nb]), ...
+                                    repmat(sx_f, [1,1,nb]), repmat(sy_f, [1,1,nb]), ...
+                                    repmat(jaci_f, [1,1,nb]), d_f, 0);
+        
+        ux_f = reshape(ux_f_mat, nL_f_total, nb); 
+        uy_f = reshape(uy_f_mat, nL_f_total, nb);
+        vx_f = reshape(vx_f_mat, nL_f_total, nb); 
+        vy_f = reshape(vy_f_mat, nL_f_total, nb);
+
+        % --- Tensor Assembly ---
+        tensor = zeros(n_test, nb, nb);
+        
+        if use_vectorized
+            % VECTORIZED VERSION: O(n_test * nb) iterations
+            % We pre-weight the test functions once
+            Phi_K_weighted_u = bsxfun(@times, Me_f, pod_u_f(:, 2:nb));
+            Phi_K_weighted_v = bsxfun(@times, Me_f, pod_v_f(:, 2:nb));
+            
+            for i = 1:nb
+                % Convection operator for fixed 'i' acting on all 'j'
+                % (u_i * d/dx + v_i * d/dy) applied to all modes j
+                conv_j_x = bsxfun(@times, pod_u_f(:,i), ux_f) + ...
+                           bsxfun(@times, pod_v_f(:,i), uy_f);
+                conv_j_y = bsxfun(@times, pod_u_f(:,i), vx_f) + ...
+                           bsxfun(@times, pod_v_f(:,i), vy_f);
+                
+                % Contract with all k test functions simultaneously
+                % Result: [n_test, nb]
+                res = (Phi_K_weighted_u' * conv_j_x) + (Phi_K_weighted_v' * conv_j_y);
+                tensor(:, :, i) = res;
+            end
+        else
+            % READABLE VERSION: O(n_test * nb^2) iterations
+            for k = 1:n_test
+                wk_u = Me_f .* pod_u_f(:, k+1);
+                wk_v = Me_f .* pod_v_f(:, k+1);
+                for i = 1:nb
+                    ui = pod_u_f(:, i); vi = pod_v_f(:, i);
+                    for j = 1:nb
+                        conv_x = ui .* ux_f(:, j) + vi .* uy_f(:, j);
+                        conv_y = ui .* vx_f(:, j) + vi .* vy_f(:, j);
+                        tensor(k, j, i) = wk_u' * conv_x + wk_v' * conv_y;
+                    end
+                end
+            end
+        end
+    end
+    
+    % --- Online Stage ---
+    T_mat = reshape(tensor, n_test * nb, nb);
+    temp = reshape(T_mat * ucoef, n_test, nb);
+    out_coef = sum(temp .* ucoef', 2);
+end
